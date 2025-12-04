58d190fdb458c3d8ca97c93d1728f12e1e5ea1d6
aravind-ponnala <aravind@zofai.com>
2025-12-03 21:34:00 -0500

    made deployment changes

--- app/main.py
+++ app/main.py
@@ -21,6 +21,7 @@ app = FastAPI(title="Message Search Engine", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["GET"],
     allow_headers=["*"],
 )
